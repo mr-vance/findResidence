c67aa933b7c6947f07b67f9c1396a57fa6dda9a2
Vance <vansmuchongo@gmail.com>
2023-07-30 01:17:19 +0000

    navigating from welcome page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,44 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, useNavigate } from 'react-router-dom';
 import './App.css';
+import Signup from './pages/signup';
+import Login from './pages/login';
 
 function App() {
+  const navigate = useNavigate();
+
   const handleSignupClick = () => {
-    // Code to navigate to the signup page
-    // For example: window.location.href = '/signup';
+    // Navigate to the signup page
+    navigate('/signup');
   };
 
   const handleLoginClick = () => {
-    // Code to navigate to the login page
-    // For example: window.location.href = '/login';
+    // Navigate to the login page
+    navigate('/login');
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Our Accommodation Platform</h1>
-        <p>Find your perfect accommodation with ease!</p>
-        <div className="buttons-container">
-          <button className="round-button signup-button" onClick={handleSignupClick}>
-            Sign Up
-          </button>
-          <button className="round-button login-button" onClick={handleLoginClick}>
-            Login
-          </button>
-        </div>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to Our Accommodation Platform</h1>
+          <p>Find your perfect accommodation with ease!</p>
+          <div className="buttons-container">
+            <button className="round-button signup-button" onClick={handleSignupClick}>
+              Sign Up
+            </button>
+            <button className="round-button login-button" onClick={handleLoginClick}>
+              Login
+            </button>
+          </div>
+        </header>
+
+        <Switch>
+          <Route path="/signup" component={Signup} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
