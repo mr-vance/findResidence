9e48a06a62e4a46f5e011cd9d5075326f7fdc45e
Vance <vansmuchongo@gmail.com>
2023-07-30 00:42:05 +0000

    welcome page working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const handleSignupClick = () => {
+    // Code to navigate to the signup page
+    // For example: window.location.href = '/signup';
+  };
+
+  const handleLoginClick = () => {
+    // Code to navigate to the login page
+    // For example: window.location.href = '/login';
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Our Accommodation Platform</h1>
+        <p>Find your perfect accommodation with ease!</p>
+        <div className="buttons-container">
+          <button className="round-button signup-button" onClick={handleSignupClick}>
+            Sign Up
+          </button>
+          <button className="round-button login-button" onClick={handleLoginClick}>
+            Login
+          </button>
+        </div>
       </header>
     </div>
   );
