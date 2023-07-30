841a9c847bd6c0da2a75199fbef3728aac4c3bc9
Vance <vansmuchongo@gmail.com>
2023-07-30 05:14:28 +0000

    login, register, all fine

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import './signup-login.css';
 
 function Login() {
   return (
-    <div>
-      <h2>Login Page</h2>
-      {/* Add your login form or content here */}
+    <div className="wrapper">
+      <h2>Login</h2>
+      <form action="#">
+        <div className="input-box">
+          <input type="text" placeholder="Enter your email" required />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Enter password" required />
+        </div>
+        <div className="input-box button">
+          <input type="submit" value="Login" />
+        </div>
+        <div className="text">
+          <h3>Forgot password? <a href="#">Reset now</a></h3>
+        </div>
+      </form>
     </div>
   );
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import './signup-login.css';
 
 function Signup() {
   return (
-    <div>
-      <h2>Signup Page</h2>
-      {/* Add your signup form or content here */}
+    <div className="wrapper">
+      <h2>Registration</h2>
+      <form action="#">
+        <div className="input-box">
+          <input type="text" placeholder="Enter your name" required />
+        </div>
+        <div className="input-box">
+          <input type="text" placeholder="Enter your email" required />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Create password" required />
+        </div>
+        <div className="input-box">
+          <input type="password" placeholder="Confirm password" required />
+        </div>
+        <div className="policy">
+          <input type="checkbox" />
+          <h3>I accept all terms & condition</h3>
+        </div>
+        <div className="input-box button">
+          <input type="submit" value="Register Now" />
+        </div>
+      </form>
     </div>
   );
 }
